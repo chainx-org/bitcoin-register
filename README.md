6f447e53ece268cf71f6dcf9c70d2e50ba62854d
toxotguo <scut-guo@163.com>
2018-10-18 15:21:26 +0800

    support btc testnet 3

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 npm install
+
 npm install -g browserify
+
 browserify -r ./bitcoin.js:bitcoin-util >bitcoin-util.js
 
 open chainx-register-btc.html
---
+++ bitcoin.js
@@ -0,0 +1,33 @@
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');//version=3.3.2
+var {bytesToHex,ss58Decode} = require('oo7-substrate');
+
+// import using testnet 3 network
+var pushtx = require('blockchain.info').pushtx.usingNetwork(3)
+var blockexplorer=require('blockchain.info').blockexplorer.usingNetwork(3)
+
+function toChainxPubkey(address){
+    var account_58 = address;//'5GoKvZWG5ZPYL1WUovuHW3zJBWBP5eT8CbqjdRY4Q6iMaDtZ';
+    var account_u8 = ss58Decode(account_58); //58地址=》u8
+    return  bytesToHex(account_u8); //公钥
+}
+
+function toBitcoinTestAdress(words){
+    let seedBuffer = bip39.mnemonicToSeed(words);
+    let seedHex = seedBuffer.toString('hex');
+    //console.log('seed='+seedHex);
+
+    let root = bitcoin.HDNode.fromSeedHex(seedHex,bitcoin.networks.testnet);
+    //console.log('xprv: ' + root.toBase58()); 
+    //console.log('xpub: ' + root.neutered().toBase58()); 
+
+    //生成派生key:
+    let child0 = root.derivePath("m/44'/1'/0'/0/0");
+
+    //console.log("prv m/44'/1'/0'/0/0: " + child0.keyPair.toWIF()); // KzuPk3PXKdnd6QwLqUCK38PrXoqJfJmACzxTaa6TFKzPJR7H7AFg
+    //console.log("pub m/44'/1'/0'/0/0: " + child0.getAddress()); // 
+
+    return {address:child0.getAddress(),wif:child0.keyPair.toWIF()};
+}
+module.exports ={toBitcoinTestAdress,toChainxPubkey}
+
