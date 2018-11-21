f6b0f340229c9c01c84a4287c6aad31e4fb8bb78
toxotguo <scut-guo@163.com>
2018-11-21 12:19:16 +0800

    add fromPrivateKeyToTestAddress (#1)
    * add fromPrivateKeyToTestAddress

--- bitcoin.js
+++ bitcoin.js
@@ -1,6 +1,7 @@
 const bip39 = require('bip39');
 const bitcoin = require('bitcoinjs-lib');//version=3.3.2
-var {bytesToHex,ss58Decode} = require('oo7-substrate');
+var {bytesToHex,ss58Decode,hexToBytes} = require('oo7-substrate');
+const bip32 = require('bip32');
 
 // import using testnet 3 network
 var pushtx = require('blockchain.info').pushtx.usingNetwork(3)
@@ -29,5 +30,19 @@ function toBitcoinTestAdress(words){
 
     return {address:child0.getAddress(),wif:child0.keyPair.toWIF()};
 }
-module.exports ={toBitcoinTestAdress,toChainxPubkey}
+function fromPrivateKeyToTestAddress(pri){
+    let root = bitcoin.HDNode.fromBase58(pri,bitcoin.networks.testnet);
+    //console.log('xprv: ' + root.toBase58()); 
+    //console.log('xpub: ' + root.neutered().toBase58()); 
+
+    //生成派生key:
+    let child0 = root.derivePath("m/44'/1'/0'/0/0");
+
+    //console.log("prv m/44'/1'/0'/0/0: " + child0.keyPair.toWIF()); // KzuPk3PXKdnd6QwLqUCK38PrXoqJfJmACzxTaa6TFKzPJR7H7AFg
+    //console.log("pub m/44'/1'/0'/0/0: " + child0.getAddress()); // 
+
+    return {address:child0.getAddress(),wif:child0.keyPair.toWIF()};
+}
+
+module.exports ={toBitcoinTestAdress,toChainxPubkey,fromPrivateKeyToTestAddress}
 
